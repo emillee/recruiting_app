1959cef106a68675903781ae06634041ca7f895c
emillee <emil.lee@stern.nyu.edu>
2014-04-18 10:14:06 -0400

    Fix for heroku

--- db/migrate/20140307170350_remove_columns_from_user.rb
+++ db/migrate/20140307170350_remove_columns_from_user.rb
@@ -1,6 +1,6 @@
 class RemoveColumnsFromUser < ActiveRecord::Migration
   def change
-    remove_column :user, :oauth_token
-    remove_column :user, :oauth_token_expires_at
+    # remove_column :user, :oauth_token
+    # remove_column :user, :oauth_token_expires_at
   end
 end
